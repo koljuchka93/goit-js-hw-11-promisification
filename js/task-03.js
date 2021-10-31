fc00385375421343d6d76b1b964d6c174eac0cb9
Kateryna <koljuchka93@gmail.com>
2021-10-31 16:15:52 +0200

    t3 fix

--- js/task-03.js
+++ js/task-03.js
@@ -3,22 +3,22 @@ const randomIntegerFromInterval = (min, max) => {
 };
 
 const makeTransaction = transaction => {
-  return new Promise((resolve, reject) => {
-    const delay = randomIntegerFromInterval(200, 500);
-  
-    setTimeout(() => {
-    const canProcess = Math.random() > 0.3;
+    return new Promise((resolve, reject) => {
+  const delay = randomIntegerFromInterval(200, 500);
 
-    if (canProcess) {
-      resolve(transaction.id, delay);
-    } else {
-      reject(transaction.id);
-    }
-  }, delay);
-  })
+        setTimeout(() => {
+            const canProcess = Math.random() > 0.3;
+
+            if (canProcess) {
+                resolve({ id: transaction.id, time: delay });
+            } else {
+                reject(transaction.id);
+            }
+        }, delay);
+    });
 };
 
-const logSuccess = ({ id, time }) => {
+const logSuccess = ({id, time}) => {
   console.log(`Transaction ${id} processed in ${time}ms`);
 };
 
@@ -26,16 +26,6 @@ const logError = id => {
   console.warn(`Error processing transaction ${id}. Please try again later.`);
 };
 
-/*
- * Работает так
- */
-// makeTransaction({ id: 70, amount: 150 }, logSuccess, logError);
-// makeTransaction({ id: 71, amount: 230 }, logSuccess, logError);
-// makeTransaction({ id: 72, amount: 75 }, logSuccess, logError);
-// makeTransaction({ id: 73, amount: 100 }, logSuccess, logError);
-/*
- * Должно работать так
- */
 makeTransaction({ id: 70, amount: 150 })
   .then(logSuccess)
   .catch(logError);
